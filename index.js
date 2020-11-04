36553cda30500ea575e4a9929718d092181d50bc
ishuraj2010@gmail.com <ishuraj2010@gmail.com>
2020-11-04 16:47:06 +0530

    timezone changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const MUSTACHE_MAIN_DIR = './main.mustache';/**
   * Notice the "name" and "date" property.
 */
 let DATA = {
-  name: 'Thomas',
+  name: 'Ishu',
   date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
     month: 'long',
@@ -15,7 +15,7 @@ let DATA = {
     hour: 'numeric',
     minute: 'numeric',
     timeZoneName: 'short',
-    timeZone: 'Europe/Stockholm',
+    timeZone: 'Indian',
   }),
 };/**
   * A - We open 'main.mustache'
